21ada0006a355d3d7a2ff19a61de26c3ff18d4e6
Nyasha <wisdomzw@gmail.com>
2019-10-23 17:13:58 +0200

    add service to get album data

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ProductService } from '../product.service';
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
+  albumInfo:any;
+  constructor(private _productService: ProductService) {
 
-  constructor() { }
+  }
 
   ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response)
   }
 
 }
