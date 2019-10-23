6b545a51d82dc44ba6f3eac3aee6ea7055010198
Nyasha <wisdomzw@gmail.com>
2019-10-23 23:20:24 +0200

    interfaces added

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
+import { Album } from "../album";
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-  albumInfo:any;
+  albumInfo:Album;
   constructor(private _productService: ProductService) {
 
   }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from "@angular/http";
+import { Album } from "./album";
+import { Observable } from "rxjs/Observable";
 
 import 'rxjs/add/operator/map';
+import { importExpr } from '@angular/compiler/src/output/output_ast';
 
 @Injectable()
 export class ProductService {
@@ -10,7 +13,7 @@ export class ProductService {
 
   private _albumUrl = '../assets/album.json';
 
-  getAlbum(id:number) {
+  getAlbum(id:number):Observable<Album> {
     return this._http.get(this._albumUrl).map((response) => response.json());
   }
 }
