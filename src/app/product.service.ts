c34713ea2f12dc32e1e1bdf650613dc27e23a00e
Nyasha <wisdomzw@gmail.com>
2019-10-25 11:44:35 +0200

    add display list of albums

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from "@angular/http";
-import { Album } from "./album";
-import { Observable } from "rxjs/Observable";
+import { Http, Response } from '@angular/http';
+import { Album } from './album';
+import { Product } from "./product";
+import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/operator/map';
 import { importExpr } from '@angular/compiler/src/output/output_ast';
@@ -12,9 +13,14 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   getAlbum(id:number):Observable<Album> {
     return this._http.get(this._albumUrl).map((response) => <Album>response.json());
+  }
+
+  getProducts():Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
   }
 }
 
